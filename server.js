cf23e74f64733eb4905809bca9bf8be3e529fbe1
Olga Kalisiak <01178853@pw.edu.pl>
2024-05-25 15:18:29 +0200

    nowa funkcjonalność - strona admina z anulowaniem rezerwacji

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
-// Set the view engine to EJS
+// Set the view engine to EJS for the admin page
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'client'));
 
 // Połączenie z bazą danych MongoDB
-mongoose.connect('mongodb://localhost:27017/rezerwacje');
+mongoose.connect('mongodb://localhost:27017/rezerwacje', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 
 // Ustawienie katalogu, który będzie serwowany statycznie
@@ -23,21 +27,41 @@ app.get('/reservation', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'reservation.html'));
 });
 
+// Endpoint dla strony potwierdzenia
 app.get('/confirmation', (req, res) => {
-  res.sendFile(path.join(__dirname, 'confirmation.html'));
+  res.sendFile(path.join(__dirname, 'client', 'confirmation.html'));
 });
 
-app.get('/admin', (req, res) => {
-  res.render(path.join(__dirname, 'client', 'admin.html'));
+// Endpoint dla strony administracyjnej
+app.get('/admin', async (req, res) => {
+  try {
+    const reservations = await Reservation.find({});
+    res.render('admin', { reservations: reservations });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Wystąpił błąd podczas pobierania rezerwacji.');
+  }
+});
+
+// Endpoint do anulowania rezerwacji
+app.delete('/cancel/:id', async (req, res) => {
+  try {
+    const reservationId = req.params.id;
+    await Reservation.findByIdAndDelete(reservationId);
+    res.status(200).send('Rezerwacja anulowana');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Wystąpił błąd podczas anulowania rezerwacji.');
+  }
 });
 
 // Definicja schematu dla danych rezerwacji
 const reservationSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    people: Number,
-    date: Date,
-    time: String
+  name: String,
+  email: String,
+  people: Number,
+  date: Date,
+  time: String
 });
 
 // Model danych rezerwacji
@@ -46,35 +70,20 @@ const Reservation = mongoose.model('Reservation', reservationSchema);
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.post('/reservation', (req, res) => {
-    try {
-        const name = req.body.name;
-        const email = req.body.email;
-        const people = req.body.people;
-        const date = req.body.date;
-        const time = req.body.time;
-        
-        // Wykonaj operacje na danych, np. zapisz do bazy danych
-        const newReservation = new Reservation({
-            name: name,
-            email: email,
-            people: people,
-            date: date,
-            time: time
-        });
+app.post('/reservation', async (req, res) => {
+  try {
+    const { name, email, people, date, time } = req.body;
 
-        // Zapisanie nowej rezerwacji do bazy danych
-        newReservation.save().then(savedReservation => {
-          res.redirect('/confirmation');
-        },
-        err => {
-            console.error(err);
-            res.status(500).send('Wystąpił błąd podczas zapisywania rezerwacji.');
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Wystąpił błąd podczas przetwarzania rezerwacji.');
-    }
+    // Wykonaj operacje na danych, np. zapisz do bazy danych
+    const newReservation = new Reservation({ name, email, people, date, time });
+
+    // Zapisanie nowej rezerwacji do bazy danych
+    await newReservation.save();
+    res.redirect('/confirmation');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Wystąpił błąd podczas przetwarzania rezerwacji.');
+  }
 });
 
 // Nasłuch na określonym porcie
